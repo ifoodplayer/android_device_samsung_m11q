5f39bf746676dc234b9bab8ffc759aa840d39b92
Ayush Bisht <ayushbisht5663@gmail.com>
2021-06-11 03:33:17 +0530

    m11q: board have ANT
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- device.mk
+++ device.mk
@@ -72,6 +72,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/m11q/m11q-vendor.mk)
 
