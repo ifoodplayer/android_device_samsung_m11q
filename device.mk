fcdcc9795621b3d98f4c3c1799ebb4a6f665e5de
Ayush Bisht <ayushbisht5663@gmail.com>
2021-06-12 03:30:07 +0530

    m11q: import keylayouts from stock
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- device.mk
+++ device.mk
@@ -157,6 +157,14 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsxv26.kl:system/usr/keylayout/synaptics_dsxv26.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
