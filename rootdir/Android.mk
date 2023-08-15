785899184335f03a257c2001182d6e44e13a4b0e
Ayush <ayushbisht5663@gmail.com>
2023-08-15 14:01:51 +0530

    m11q: fix LOCAL_MODULE_STEM
    Signed-off-by: Ayush <ayushbisht5663@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.boot.qcom
-+LOCAL_MODULE_STEM := fstab.qcom
+LOCAL_MODULE_STEM := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.boot.qcom
