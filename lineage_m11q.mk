e0d076e681fbebfbc92df7ce616b8a6a33c73a32
ifoodplayer <joaogamer1437@gmail.com>
2025-12-05 14:06:53 -0300

    m11q: Import lib64 from a20s
    Change-Id: I807a941f5c2a3b81ed7614b0e925e272a431ccaa

--- lineage_m11q.mk
+++ lineage_m11q.mk
@@ -14,7 +14,7 @@
 
 # Inherit from those products, Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from m11q device
 $(call inherit-product, device/samsung/m11q/device.mk)
