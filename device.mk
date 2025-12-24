620f07c2c243032f20ddcaec84dc9f77e06108ea
ifoodplayer <joaogamer1437@gmail.com>
2025-12-23 22:13:50 -0300

    m11q: import lights hal from hardware/qcom-caf
    disable import of lights.msm8953.so

--- device.mk
+++ device.mk
@@ -264,7 +264,8 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service \
+    lights.msm8953
 
 # Vibrator
 PRODUCT_PACKAGES += \
