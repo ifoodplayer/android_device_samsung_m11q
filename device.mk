dd046ecd97dacc7ef599a125e082923a4ea75b96
ifoodplayer <joaogamer1437@gmail.com>
2026-02-20 17:39:34 -0300

    m11q: Use Samsung Vibrator instead of AOSP
    * HAL has not stopped working yet, so we will use it to increase compatibility in GSIs that were not work vibration

--- device.mk
+++ device.mk
@@ -233,8 +233,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.3.vendor:32
 
 # Capability Configstore
 PRODUCT_PACKAGES += \
