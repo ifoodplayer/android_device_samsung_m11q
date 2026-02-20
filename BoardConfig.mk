9e2e4f7afe84d3ceb49d3bde483549b0b292c439
ifoodplayer <joaogamer1437@gmail.com>
2026-02-20 17:45:27 -0300

    m11q: set TARGET_SCREEN_DENSITY in tree
    * the prop has already been nuked

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,9 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Display
+TARGET_SCREEN_DENSITY := 280
+
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
