cf3993819ee551e946d41511b4eccc9997733082
ifoodplayer <joaogamer1437@gmail.com>
2026-03-03 13:38:17 -0300

    m11q: Partially merge lineage-20 into lineage-19.1

--- device.mk
+++ device.mk
@@ -38,7 +38,6 @@ TARGET_SCREEN_HEIGHT := 1560
 TARGET_SCREEN_WIDTH := 720
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_BUILD_SUPER_PARTITION := false
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
@@ -384,6 +383,17 @@ PRODUCT_COPY_FILES += \
 # Seccomp policies
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
+
+# For userdebug builds
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	ro.debuggable=1 \
+	persist.sys.root_access=1 \
+	persist.service.adb.enable=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=adb
 
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/m11q/m11q-vendor.mk)
