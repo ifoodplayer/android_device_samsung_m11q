c8423070fe467f0f0ccfa42b1fdafbc78818023c
ifoodplayer <joaogamer1437@gmail.com>
2026-03-21 20:24:51 -0300

    m11q: Inherit from QTI FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,9 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+
 # Display
 TARGET_SCREEN_DENSITY := 280
 
