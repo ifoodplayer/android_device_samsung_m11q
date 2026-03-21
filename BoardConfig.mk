2fba2e3a6e752d016e13c8cd598fc63fc17ccf24
ifoodplayer <joaogamer1437@gmail.com>
2026-03-21 20:31:50 -0300

    m11q: Add a device specific FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,6 +187,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+	$(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 # Display
