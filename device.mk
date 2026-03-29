8bc85a4c6bf08de291a164d9aeead9e19ecfa1e0
ifoodplayer <joaogamer1437@gmail.com>
2026-03-29 17:31:19 -0300

    m11q: Remove custom power qti
    - Double to wake is not working in AOSP, so we will remove it

--- device.mk
+++ device.mk
@@ -257,7 +257,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti-m11q \
+    android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor:32
 
